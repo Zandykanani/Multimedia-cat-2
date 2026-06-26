2f8f92f1c814408ecef74a470012d42f8cc8608a
Zandykanani <166380925+Zandykanani@users.noreply.github.com>
2026-06-26 15:24:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleVideo(){
+
+let video=document.getElementById("wildlifeVideo");
+
+if(video.style.display==="none"){
+
+video.style.display="block";
+video.play();
+
+}
+
+else{
+
+if(video.paused){
+
+video.play();
+
+}
+
+else{
+
+video.style.display="none";
+
+}
+
+}
+
+}
